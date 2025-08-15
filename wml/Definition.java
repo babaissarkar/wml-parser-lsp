4ab421aa1d2183617fc5a41124334962f2edb536
Subhraman Sarkar <suvrax@gmail.com>
2025-08-15 22:26:29 +0530

    Definition: support default arguments

--- wml/Definition.java
+++ wml/Definition.java
@@ -1,11 +1,13 @@
 package wml;
 
+import java.util.HashMap;
 import java.util.Vector;
 
 public class Definition {
 	private String name;
 	private String value;
 	private Vector<String> args = new Vector<>();
+	private HashMap<String, String> defArgs = new HashMap<>();
 	
 	public Definition(String name, String value) {
 		this.name = name;
@@ -18,8 +20,19 @@ public class Definition {
 		this.args = args;
 	}
 	
+	public Definition(String name, String value, Vector<String> args, HashMap<String, String> defArgs) {
+		this.name = name;
+		this.value = value;
+		this.args = args;
+		this.defArgs = defArgs;
+	}
+	
 	public void addArg(String arg) {
 		args.add(arg);
+	}
+	
+	public void addDefArg(String key, String val) {
+		defArgs.put(key, val);
 	}
 	
 	public String getValue() {
@@ -27,7 +40,7 @@ public class Definition {
 	}
 	
 	/** Expand the macro, substituting any given args */
-	public String expand(Vector<String> values) {
+	public String expand(Vector<String> values, HashMap<String, String> keyVals) {
 		String unparsed = this.value;
 		if (values.size() != args.size()) {
 			throw new IllegalArgumentException("Wrong number of arguments supplied to macro " + this.name);
@@ -38,7 +51,19 @@ public class Definition {
 			unparsed = unparsed.replace("{" + arg + "}", values.get(i));
 			i++;
 		}
+		
+		for (var entry : defArgs.entrySet()) {
+			String val = keyVals.get(entry.getKey());
+			if (val == null) {
+				val = entry.getValue();
+			}
+			unparsed = unparsed.replace("{" + entry.getKey() + "}", val);
+		}
 		return unparsed;
+	}
+	
+	public String expand(Vector<String> values) {
+		return expand(values, new HashMap<String, String>());
 	}
 	
 	public String toString() {
