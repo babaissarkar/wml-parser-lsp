c1a5b1927ccabe745a8078786635811549365df7
Subhraman Sarkar <suvrax@gmail.com>
2025-10-06 13:07:12 +0530

    Improve FS resolution, add unit test
    * still needs more work for addon paths

--- src/main/java/com/babai/wml/utils/FS.java
+++ src/main/java/com/babai/wml/utils/FS.java
@@ -2,33 +2,53 @@ package com.babai.wml.utils;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class FS {
 	private FS() {
 	}
+	
+	public static String getAssetType(String path) {
+		// TODO: add sounds folder if needed
+		var extFolders = Map.of(
+			List.of(".png", ".jpg", ".webp"),"images",
+			List.of(".ogg"), "music",
+			List.of(".wav"), "sounds"
+		);
+		var coreFolders = extFolders.values().stream().collect(Collectors.toSet());
+		for (var entry : extFolders.entrySet()) {
+			if (entry.getKey().stream().anyMatch(path::endsWith)) {
+				return entry.getValue();
+			}
+		}
+		return "";
+	}
 
 	/** Convert Wesnoth path string to NIO Path object */
 	public static Path resolve(String pathStr, Path currentPath, Path dataPath, Path userDataPath) {
-		Path p;
+		Path parent = null;
+
 		if (pathStr.startsWith(".")) {
-			if (Files.isDirectory(currentPath)) {
-				p = currentPath.resolve(pathStr);
-			} else {
-				p = currentPath.getParent().resolve(pathStr);
-			}
-		} else if (pathStr.startsWith("~")) {
-			// TODO should throw warning if userdatapath is null
-			// Supports both ~add-ons and ~/add-ons
-			String relpath = pathStr.substring(1);
-			if (relpath.startsWith("/")) {
-				relpath = relpath.substring(1);
-			}
-			p = userDataPath.resolve(relpath);
+			parent = Files.isDirectory(currentPath) ? currentPath : currentPath.getParent();
 		} else {
-			// TODO should throw warning if datapath is null
-			p = dataPath.resolve(pathStr);
+			String assetType = getAssetType(pathStr); 
+			pathStr = Path.of(assetType, pathStr).toString();
+			if (pathStr.startsWith("~")) {
+				// Supports both ~add-ons and ~/add-ons
+				pathStr = pathStr.replaceFirst("^~/?", "");
+				parent = userDataPath;
+			} else {
+				// E.g.: scenary/alter.png
+				if (!assetType.isEmpty()) {
+					pathStr = Path.of("core", pathStr).toString();
+				}
+				parent = dataPath;
+			}
 		}
 
-		return p;
+		return parent != null ? parent.resolve(pathStr).normalize() : parent;
 	}
 }
