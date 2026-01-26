13b81ffccb9a7b5719c5f61032bc98c4983ba984
Subhraman Sarkar <suvrax@gmail.com>
2026-01-26 14:57:06 +0530

    EOL token

--- src/main/java/com/babai/wml/experimental/Tokenizer.java
+++ src/main/java/com/babai/wml/experimental/Tokenizer.java
@@ -1,7 +1,8 @@
 package com.babai.wml.experimental;
 
-import java.io.BufferedReader;
+import java.io.Reader;
 import java.io.IOException;
+import java.io.PushbackReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -12,10 +13,14 @@ public final class Tokenizer {
 		return tokenize(Files.newBufferedReader(inputPath));
 	}
 	
-	public static List<Token> tokenize(BufferedReader r) throws IOException {
+	// TODO: macro calls: {MYMACRO ARG1 ARG2 ...}, quoted text
+	public static List<Token> tokenize(Reader reader) throws IOException {
+		PushbackReader r = new PushbackReader(reader);
 		List<Token> tokens = new ArrayList<>();
 		StringBuilder buff = new StringBuilder();
 		State state = State.NORMAL;
+		
+		// No state change needed in case of EOL
 		
 		int ch;
 		while((ch = r.read()) != -1) {
@@ -23,22 +28,45 @@ public final class Tokenizer {
 			switch(state) {
 				case NORMAL -> {
 					if (c == '#') {
-						tokens.add(new Token(buff.toString(), Token.Kind.TEXT));
-						buff.delete(0, buff.length());
-						
+						finalizeAndAddToken(tokens, buff, Token.Kind.TEXT);
 						state = State.LINE_COMMENT;
+						buff.append(c);
+					} else if (isWS(c)) {
+						finalizeAndAddToken(tokens, buff, Token.Kind.TEXT);
+						state = State.WS;
+						buff.append(c);
+					} else if (isEOL(c)) {
+						handleEOLToken(tokens, c, r);
+					} else {
+						buff.append(c);
 					}
-					buff.append(c);
 				}
 				
 				case LINE_COMMENT -> {
 					if (isEOL(c)) {
-						tokens.add(new Token(buff.toString(), Token.Kind.COMMENT));
-						buff.delete(0, buff.length());
-						
+						finalizeAndAddToken(tokens, buff, Token.Kind.COMMENT);
+						handleEOLToken(tokens, c, r);
 						state = State.NORMAL;
+					} else {
+						buff.append(c);
 					}
-					buff.append(c);
+				}
+				
+				case WS -> {
+					if (!isWS(c)) {
+						finalizeAndAddToken(tokens, buff, Token.Kind.WHITESPACE);
+						if (c == '#') {
+							state = State.LINE_COMMENT;
+						} else {
+							state = State.NORMAL;
+						}
+					}
+					
+					if (isEOL(c)) {
+						handleEOLToken(tokens, c, r);
+					} else {
+						buff.append(c);
+					}
 				}
 			}
 		}
@@ -48,15 +76,37 @@ public final class Tokenizer {
 			// file terminated in the middle of content, finish tokens
 			// TODO maybe throw exception to warn about the issue.
 			if (state == State.NORMAL) {
-				tokens.add(new Token(buff.toString(), Token.Kind.TEXT));
-				buff.delete(0, buff.length());
+				finalizeAndAddToken(tokens, buff, Token.Kind.TEXT);
 			} else if (state == State.LINE_COMMENT) {
-				tokens.add(new Token(buff.toString(), Token.Kind.COMMENT));
-				buff.delete(0, buff.length());
+				finalizeAndAddToken(tokens, buff, Token.Kind.COMMENT);
+			} else if (state == State.WS) {
+				finalizeAndAddToken(tokens, buff, Token.Kind.WHITESPACE);
 			}
 		}
 		
 		return tokens;
+	}
+	
+	private static void handleEOLToken(List<Token> tokens, char c, PushbackReader r) throws IOException {
+		if (c == '\r') {
+			char c2 = (char) r.read();
+			if (c2 == '\n') {
+				finalizeAndAddToken(tokens, "\r\n", Token.Kind.EOL);
+			} else {
+				r.unread(c2);
+			}
+		} else {
+			finalizeAndAddToken(tokens, "" + c, Token.Kind.EOL);
+		}
+	}
+
+	private static void finalizeAndAddToken(List<Token> tokens, String contents, Token.Kind kind) {
+		tokens.add(new Token(contents, kind));
+	}
+
+	private static void finalizeAndAddToken(List<Token> tokens, StringBuilder buff, Token.Kind kind) {
+		tokens.add(new Token(buff.toString(), kind));
+		buff.delete(0, buff.length());
 	}
 	
 	private static boolean isEOL(char c) {
@@ -68,5 +118,5 @@ public final class Tokenizer {
 	}
 
 	
-	private enum State { NORMAL, LINE_COMMENT };
+	private enum State { NORMAL, LINE_COMMENT, WS };
 }
