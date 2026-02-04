29f068d6d67b5a19f09f4fc91f18e09f76742b67
Subhraman Sarkar <suvrax@gmail.com>
2026-02-04 16:40:16 +0530

    Support quoted strings and add corresponding test

--- src/test/java/com/babai/wml/TokenizerTest.java
+++ src/test/java/com/babai/wml/TokenizerTest.java
@@ -19,7 +19,7 @@ class TokenizerTest {
 		String text = "Hello #Comment\n";
 		try {
 			List<Token> toks = Tokenizer.tokenize(new BufferedReader(new StringReader(text)));
-			System.out.println("Toks: " + toks);
+			System.out.println("Toks(comment test): " + toks);
 			assertEquals(toks.size(), 4);
 			assertEquals(toks.get(0).getContent(), "Hello");
 			assertEquals(toks.get(1).getContent(), " ");
@@ -30,4 +30,17 @@ class TokenizerTest {
 		}
 	}
 
+	@Test
+	void testQuotedString() {
+		String text = "key=\"value val\"\"ue2\nvalue3\"";
+		try {
+			List<Token> toks = Tokenizer.tokenize(new BufferedReader(new StringReader(text)));
+			System.out.println("Toks(quoted test): " + toks);
+			assertEquals(toks.size(), 1);
+			// checks 1. "" -> " collapse, preservation of whitespace
+			assertEquals(toks.get(0).getContent(), "key=\"value val\"ue2\nvalue3\"");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
 }
