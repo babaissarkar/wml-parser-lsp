22d43517029f2039bf7ed6f17e626bfc1481f215
Subhraman Sarkar <suvrax@gmail.com>
2026-03-07 17:20:03 +0530

    Preprocessor: add test

---
+++ src/test/java/com/babai/wml/PreprocessorTest.java
@@ -0,0 +1,43 @@
+package com.babai.wml;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import com.babai.wml.core.Definition;
+import com.babai.wml.experimental.Preprocessor;
+
+class PreprocessorTest {
+
+	@Test
+	void testDefineBasic() {
+		String defString = """
+			#define MYMACRO ARG1 ARG2
+			#arg DARG1
+			default#endarg
+			
+			#arg DARG2
+			default2#endarg
+			
+			#arg DARG3
+			default3#endarg
+			Something#enddef""";
+		try {
+			Preprocessor.preprocess(new StringReader(defString));
+			var defines = Preprocessor.getDefines();
+			assertEquals(1, defines.rowCount()); // Only 1 macro defined
+			var rows = defines.getRows("Name", "MYMACRO");
+			assertEquals(1, rows.size()); // Can be retrieved
+			var macroDefinition = (Definition) rows.get(0).getColumn("Definition").getValue();
+			assertEquals(2, macroDefinition.getArgCount()); // Has 2 positional args
+			assertEquals(3, macroDefinition.getDefArgCount()); // Has 3 default args
+			System.out.println(macroDefinition);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
