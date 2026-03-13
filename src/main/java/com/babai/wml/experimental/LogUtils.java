2b00367770e631330529a2ffd5f1f36c7cf13f70
Subhraman Sarkar <suvrax@gmail.com>
2026-03-13 10:50:16 +0530

    LogUtils: add logging initialization

--- src/main/java/com/babai/wml/experimental/LogUtils.java
+++ src/main/java/com/babai/wml/experimental/LogUtils.java
@@ -1,5 +1,8 @@
 package com.babai.wml.experimental;
 
+import java.awt.Color;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import com.babai.wml.utils.Colors;
 
@@ -7,11 +10,39 @@ import static com.babai.wml.utils.ANSIFormatter.*;
 
 final class LogUtils {
 	private final static Logger pL = Logger.getLogger("preprocessor.parse");
-	private static boolean showParseLogs;
-	private static boolean warnParseLogs;
-	private static boolean disableErrors;
+	
+	// TODO setters needed
+	private static boolean showParseLogs = true;
+	private static boolean warnParseLogs = true;
+	private static boolean disableErrors = false;
+	
+	static {
+		setLoggingFormat();
+	}
 	
 	private LogUtils() {}
+	
+	private static void setLoggingFormat() {
+		for (var handler : Logger.getLogger("").getHandlers()) {
+			handler.setFormatter(new java.util.logging.Formatter() {
+				@Override
+				public String format(LogRecord r) {
+					// Customize Message for separators between Level and Message
+					Level l = r.getLevel();
+					String lvlStr = "[" + l + "]";
+					if (l == Level.SEVERE) {
+						lvlStr = colorify(lvlStr, Color.RED);
+					} else if (l == Level.WARNING) {
+						lvlStr = colorify(lvlStr, Color.ORANGE);
+					} else if (l == Level.INFO) {
+						lvlStr = colorify(lvlStr, Color.CYAN);
+					}
+
+					return lvlStr + " " + r.getMessage() + "\n";
+				}
+			});
+		}
+	}
 	
 	@SuppressWarnings("unused")
 	private static void debugPrintTokJ(Token t) {
